511876c216fc03b628d976a48dadb35f9baff880
Lincoln Zocateli <lzocateli00@outlook.com>
2022-04-06 09:10:50 -0300

    Update RunAgent.ps1

--- base-image/agent/RunAgent.ps1
+++ base-image/agent/RunAgent.ps1
@@ -9,11 +9,6 @@ param(
 
 Write-Host "Starting: $($MyInvocation.MyCommand.Definition)"
 
-[Environment]::SetEnvironmentVariable("AZP_TOKEN", '', 'Machine')
-[Environment]::SetEnvironmentVariable("AZP_TOKEN", '', 'User')
-[Environment]::SetEnvironmentVariable("AZP_AGENT_NAME", '', 'Machine')
-[Environment]::SetEnvironmentVariable("AZP_AGENT_NAME", '', 'User')
-
 Set-Location $pathAgent
     
 Write-Host "Executing run-docker.sh in $PWD ..."
